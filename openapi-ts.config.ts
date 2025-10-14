3c6a5ac2e0499462c4f74d17ea7d04fa3d09f50e
Jan Sekułowicz <sekulowicz.jan@gmail.com>
2025-10-14 23:08:28 +0200

    chore: generate openapi-ts files

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@hey-api/openapi-ts'
 
 export default defineConfig({
-  input: '.openapi/openapi.json',
-  output: 'src/contract',
+  input: './openapi/openapi.json',
+  output: './src/contract',
   client: 'fetch',
 })
