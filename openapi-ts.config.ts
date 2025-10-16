0e68f4f72f2e81f106fe4da7d9ab05ada9f14b8f
Jan Sekułowicz <sekulowicz.jan@gmail.com>
2025-10-16 21:38:28 +0200

    WIP before only referencing schemas in the paths/*.json files

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -2,6 +2,27 @@ import { defineConfig } from '@hey-api/openapi-ts'
 
 export default defineConfig({
   input: './openapi/openapi.json',
-  output: './src/api',
-  client: 'fetch',
+  output: {
+    path: './src/api/generated',
+    format: 'prettier',
+  },
+  plugins: [
+    {
+      name: '@hey-api/typescript',
+      enumStyle: 'asConst',
+      inferTypes: true,
+      output: {
+        name: 'ApiTypes',
+      },
+    },
+    {
+      name: '@hey-api/client-fetch',
+      output: {
+        name: 'ApiClient',
+      },
+    },
+  ],
+  resolver: {
+    baseDir: 'openapi',
+  },
 })
