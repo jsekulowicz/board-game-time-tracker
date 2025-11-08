821ad6ec5c4607ea6bf9f20b353d4b03c2a4fc2e
Jan Sekułowicz <sekulowicz.jan@gmail.com>
2025-11-08 16:18:45 +0100

    fix: lint

--- src/components/ui/ui-form/UiFormLabel.vue
+++ src/components/ui/ui-form/UiFormLabel.vue
@@ -2,24 +2,16 @@
 import type { LabelProps } from 'reka-ui'
 import type { HTMLAttributes } from 'vue'
 import { cn } from '@/lib/utils'
-import { Label } from '@/components/ui/label'
+import { UiLabel } from '@/components/ui/ui-label'
 import { useFormField } from './useFormField'
 
-const props = defineProps<LabelProps & { class?: HTMLAttributes["class"] }>()
+const props = defineProps<LabelProps & { class?: HTMLAttributes['class'] }>()
 
 const { error, formItemId } = useFormField()
 </script>
 
 <template>
-  <Label
-    data-slot="form-label"
-    :data-error="!!error"
-    :class="cn(
-      'data-[error=true]:text-destructive',
-      props.class,
-    )"
-    :for="formItemId"
-  >
+  <UiLabel data-slot="form-label" :data-error="!!error" :class="cn('data-[error=true]:text-destructive', props.class)" :for="formItemId">
     <slot />
-  </Label>
+  </UiLabel>
 </template>
